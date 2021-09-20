53d1f03e2fbbd7f8937437d82f4277591acfdb1d
MeerimI <irismamatovameerim@gmail.com>
2021-09-20 14:42:51 +0600

    added admin-panel for site

--- portfolio/main/admin.py
+++ portfolio/main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.db import models
+from .models import SendMessage
 
 # Register your models here.
+
+class SendMessageAdmin(admin.ModelAdmin):
+    list_display = ("pk", "email", "phone_number", "message", "sent_at")
+    search_fields = ("email",)
+    list_editable = ("message",)
+    list_filter = ("sent_at",)
+    ordering = ("sent_at",)
+
+
+admin.site.register(SendMessage, SendMessageAdmin)
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from os import pardir
 from django.contrib import admin
 from django.urls import path
-from main.views import index
+from main.views import index, formMessage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('formMessage/', formMessage, name='formMessage' )
 
 ]
